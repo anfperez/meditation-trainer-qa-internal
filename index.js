5953a2d5369cedb07f1c4b7e7baca9481e2dc0aa
Angela Perez <angela.perez@flatironschool.com>
2018-09-04 23:42:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,23 @@
 header 
 <h1>
 
-Tabs/
+Tabs/Preset Background
+- <nav>, <h1>, <p>, <radio>
+
+Form
+<form>, <input type="text">, <label>
+
+Display
+<div>
+<h1 id="names"></h1>
+<p>
+<h3>
+</div>
+
+css 
+display: flex/grid
+if grid, grid-template-columns: 1fr 1fr
+if flex, flex-wrap: wrap 
+if float, float: left
+
+*/
